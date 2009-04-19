ef769ccd58cecd23ab00d1bed733507ce2fceb99
Rickard Lindroth <rickard@black.local>
2009-04-19 16:02:03 +0200

    Added a simple test case

---
+++ test/WaveGenTest.cpp
@@ -0,0 +1,16 @@
+/*
+ *  WaveGenTest.cpp
+ *  Simpleton
+ *
+ *  Created by Rickard Lindroth on 4/19/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "gtest/gtest.h"
+#include "WaveGen.h"
+
+TEST(WaveGen, testSomething) {
+  ASSERT_TRUE(false);
+}
+
